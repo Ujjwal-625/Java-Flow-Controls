059d6fcf1bdb4a6813582bd7d5129426f8f1c080
Ujjwal-625 <ujjwalrawat248@gmail.com>
2025-03-02 13:52:12 +0530

    [Author:Ujjwal] ADD:PowerWhile.java

--- PowerWhile.java
+++ PowerWhile.java
@@ -4,8 +4,10 @@ public class PowerWhile {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         // Your code here
-        System.out.println("Enter the base ");
+        System.out.println("Enter the base , base should be positive and less than 100");
         int base=sc.nextInt();
+
+        if(base>0){
         System.out.println("Enter the exponent ");
         int exponent=sc.nextInt();
 
@@ -16,6 +18,7 @@ public class PowerWhile {
             i++;
         }
         System.out.println(base+"^"+exponent+" = "+ans);
+    }
         sc.close();
     }
 }
