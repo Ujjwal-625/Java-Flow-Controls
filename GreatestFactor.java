f1cba36f3a9dc2344f13a3445bd97f27e7411a11
Ujjwal-625 <ujjwalrawat248@gmail.com>
2025-03-02 12:26:51 +0530

    [Author:Ujjwal] ADD:GreatestFactor.java

---
+++ GreatestFactor.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class GreatestFactor {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // Your code here
+
+        System.out.println("Enter the number whose greatest factor you want to find ");
+
+        int num=sc.nextInt();
+
+
+        int ans=1;
+
+        int i=2;
+        int temp=num;
+        System.out.println("Factors of "+ num+" are ");
+        while(num!=1 && i<=num){
+            if(num%i==0){
+                while(num%i==0){
+                    System.out.print(i+" ");
+                    ans=Math.max(i,ans);
+                    num=num/i;
+                }
+            }
+            i++;
+        }
+
+        System.out.println("\nThe greatest factor of "+temp+" is "+ans);
+
+        sc.close();
+    }
+}
