6c5d2fd71a351a2a81ae9c93a61d631aa78dab60
Ujjwal-625 <ujjwalrawat248@gmail.com>
2025-03-03 09:56:55 +0530

    [Author:Ujjwal MOD:Day.java

--- Day.java
+++ Day.java
@@ -23,6 +23,32 @@ public class Day {
 
         int result = dayOfWeek(m, d, y);
 
-        System.out.println(result);
+        switch (result) {
+            case 6:
+                System.out.println("Sunday");
+                break;
+            case 0:
+                System.out.println("Monday");
+                break;
+            case 1:
+                System.out.println("Tuesday");
+                break;
+            case 2:
+                System.out.println("Wednesday");
+                break;
+            case 3:
+                System.out.println("Thursday");
+                break;
+            case 4:
+                System.out.println("Friday");
+                break;
+            case 5:
+                System.out.println("Saturday");
+                break;
+        
+            default:
+                break;
+        }
+
     }
 }
